745c9eee518992e6d18ef74938415f4898bc0a06
Isaac Kittrell <i_k35@txstate.edu>
2024-05-28 14:59:07 -0500

    used anonymous function in the callback for the event listener to reduce code

--- script.js
+++ script.js
@@ -3,26 +3,22 @@ const minus = document.querySelectorAll(".minus-img")
 const questions = document.querySelectorAll(".question")
 const answers = document.querySelectorAll(".answer")
 
+function toggleHidden(i) {
+    plus[i].classList.toggle("hidden")
+    minus[i].classList.toggle("hidden")
+    answers[i].classList.toggle("hidden")
+}
+
 for (let i = 0; i < plus.length; i++) {
-    plus[i].addEventListener("click", () => {
-        plus[i].classList.toggle("hidden")
-        minus[i].classList.toggle("hidden")
-        answers[i].classList.toggle("hidden")
+    plus[i].addEventListener("click", function() {
+        toggleHidden(i)
     })
-}
 
-for (let i = 0; i < minus.length; i++) {
-    minus[i].addEventListener("click", () => {
-        plus[i].classList.toggle("hidden")
-        minus[i].classList.toggle("hidden")
-        answers[i].classList.toggle("hidden")
+    minus[i].addEventListener("click", function() {
+        toggleHidden(i)
     })
-}
 
-for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener("click", () => {
-        plus[i].classList.toggle("hidden")
-        minus[i].classList.toggle("hidden")
-        answers[i].classList.toggle("hidden")
+    questions[i].addEventListener("click", function() {
+        toggleHidden(i)
     })
 }
